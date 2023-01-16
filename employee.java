347ebd88cd5d1894a09a72483660542233159380
vasu <vasuummidi82@gmail.com>
2023-01-16 11:49:38 +0000

    updated employee.java

--- employee.java
+++ employee.java
@@ -1,3 +1,9 @@
 Hello Guys
 This is Java class...
+<<<<<<< HEAD
 narendra has joined recently
+=======
+vasu has changed some code 
+IN this java file.
+this is for reverting 
+>>>>>>> ecfd34a (adding gitignore file)
